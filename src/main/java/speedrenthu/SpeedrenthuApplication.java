5b9933f0d28edc6e7c22aaa221a2db2f1355baea
djtesla <djtesla@gmailcom>
2021-08-01 18:47:49 +0200

    projekt és repo létrehozása

---
+++ src/main/java/speedrenthu/SpeedrenthuApplication.java
@@ -0,0 +1,13 @@
+package speedrenthu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedrenthuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeedrenthuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/speedrenthu/SpeedrenthuApplicationTests.java
@@ -0,0 +1,13 @@
+package speedrenthu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedrenthuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
